c3e7b5c7638529876b2828cd29ee089e0cfbe3fb
Gaurav Makhija <makhijagaurav98@gmail.com>
2021-04-13 17:42:16 +0530

    Updated Table Definition

--- src/main/java/com/bodogo/app/model/ProductCategory.java
+++ src/main/java/com/bodogo/app/model/ProductCategory.java
@@ -3,21 +3,34 @@ package com.bodogo.app.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.Date;
 
+@Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity
 @Table
 
 public class ProductCategory {
     @Id
     @GeneratedValue
+    @Column(nullable = false)
     private int id;
     private String name;
+
+    @CreationTimestamp
+    @Column(nullable = false,columnDefinition = "timestamp default CURRENT_TIMESTAMP")
+    private Date createdAt = new Date();
+
+    @UpdateTimestamp
+    @Column(nullable = false,columnDefinition = "timestamp default CURRENT_TIMESTAMP")
+    private Date updatedAt = new Date();
+
+    @Column(nullable = false,columnDefinition = "int default 0")
+    private int deleted=0;
+
 }
